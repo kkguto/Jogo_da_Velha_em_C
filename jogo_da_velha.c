cf3e888e8d3bd07779e910bac4e0f4622184a5f1
Gustavo Melo <160752765+kkguto@users.noreply.github.com>
2024-12-03 10:32:54 -0300

    Update jogo_da_velha.c

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -1,10 +1,3 @@
-/*
-Criando um tabuleiro de Jogo da Velha:
-
-Crie um tabuleiro de jogo da velha, usando uma matrizes de caracteres (char) 3�3, onde o usu�rio pede o n�mero da linha (1 at� 3) e o da coluna (1 at� 3). 
-A cada vez que o usu�rio entrar com esses dados, colocar um �X� ou �O� no local selecionado.
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
